7713cfa1fcbf62302bff6930d1473c67172f2033
Prince Sinha <sinhaprince013@gmail.com>
2020-10-25 15:12:26 +0530

    Updates

---
+++ precache-manifest.e114ea6acedb68dcf7afe0396e193951.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c36c9a5067ac9117c0823d47274c4113",
+    "url": "/lucky-draw/index.html"
+  },
+  {
+    "revision": "9845cc23e83fe509cd8e",
+    "url": "/lucky-draw/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "4c3081deefd9af9d8b25",
+    "url": "/lucky-draw/static/css/main.ec028fca.chunk.css"
+  },
+  {
+    "revision": "9845cc23e83fe509cd8e",
+    "url": "/lucky-draw/static/js/2.46025b15.chunk.js"
+  },
+  {
+    "revision": "1473bd3801ed56e8a5830e43331f04f4",
+    "url": "/lucky-draw/static/js/2.46025b15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c3081deefd9af9d8b25",
+    "url": "/lucky-draw/static/js/main.bcd4d233.chunk.js"
+  },
+  {
+    "revision": "15c5241a0940bcd11b37",
+    "url": "/lucky-draw/static/js/runtime-main.b38a517b.js"
+  },
+  {
+    "revision": "6afbd7b319bc1800858b4ca9509d8446",
+    "url": "/lucky-draw/static/media/ipfs-logo.6afbd7b3.png"
+  },
+  {
+    "revision": "48f8ceaf2980421026289eb1bb9071c0",
+    "url": "/lucky-draw/static/media/logo.48f8ceaf.png"
+  },
+  {
+    "revision": "5be09fdb55ac1345d196d1cb64ca820f",
+    "url": "/lucky-draw/static/media/matic-logo.5be09fdb.png"
+  },
+  {
+    "revision": "e9ada72b93b9cdb343c1f49bfa413c29",
+    "url": "/lucky-draw/static/media/metamask.e9ada72b.png"
+  },
+  {
+    "revision": "bf5d695cd39cff89ab75b055b78ce386",
+    "url": "/lucky-draw/static/media/no.bf5d695c.png"
+  },
+  {
+    "revision": "e4ba3da39e7faeda571d4d49384b9680",
+    "url": "/lucky-draw/static/media/yes.e4ba3da3.png"
+  }
+]);
